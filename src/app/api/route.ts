ff56061c6adbef3439ac3ff483b6e3c45f3eb5df
Bagus Oktaviadi <bagusoktaviadi1@gmail.com>
2025-05-26 11:16:01 +0700

    [update] token route APIs

---
+++ src/app/api/route.ts
@@ -0,0 +1,11 @@
+
+export async function POST() {
+    return Response.json({
+        "responseCode": "2007300",
+        "responseMessage": "Successful",
+        "accessToken": "eyJhbGciOiJSUzI1NiJ9.eyJleHAiOjE2NjUxMjc3OTEsIm5iZiI6MTY2NTEyNjg5MSwiaXNzIjoiRE9LVSIsImlhdCI6",
+        "tokenType": "Bearer",
+        "expiresIn": 900,
+        "additionalInfo": ""
+    })
+}
