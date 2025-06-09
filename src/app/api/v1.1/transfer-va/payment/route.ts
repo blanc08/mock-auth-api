8dc4bea01c8076ae767c5b889b0d47ece822d871
Bagus Oktaviadi <bagusoktaviadi1@gmail.com>
2025-06-09 14:56:36 +0700

    [update] no cache payment notif

--- src/app/api/v1.1/transfer-va/payment/route.ts
+++ src/app/api/v1.1/transfer-va/payment/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export async function POST() {
     return NextResponse
         .json(
@@ -7,6 +10,13 @@ export async function POST() {
                 responseCode: "4042512",
                 responseMessage: "Bill not found",
             },
-            { status: 400 }
+            {
+                status: 400,
+                headers: {
+                    'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
+                    'Pragma': 'no-cache',
+                    'Expires': '0'
+                }
+            }
         );
 }
