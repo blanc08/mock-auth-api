6b954f0e1fdf0dc3741cdeac89ed2ed28b7395b1
Bagus Oktaviadi <bagusoktaviadi1@gmail.com>
2025-06-10 15:02:19 +0700

    [update] success notification response update

--- src/app/api/v1.1/transfer-va/payment/route.ts
+++ src/app/api/v1.1/transfer-va/payment/route.ts
@@ -4,19 +4,29 @@ export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export async function POST() {
-    return NextResponse
-        .json(
-            {
-                responseCode: "4042512",
-                responseMessage: "Bill not found",
+    return NextResponse.json({
+        responseCode: "2002500",
+        responseMessage: "Success",
+        virtualAccountData: {
+            partnerServiceId: "   19008",
+            customerNo: "00000000000000000001",
+            virtualAccountNo: "  19008000000000000000000001",
+            virtualAccountName: "Customer Name",
+            virtualAccountEmail: "customer.email@mail.com",
+            paymentRequestId: "12839218738127830",
+            paidAmount: {
+                value: "11500.00",
+                currency: "IDR"
             },
-            {
-                status: 400,
-                headers: {
-                    'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
-                    'Pragma': 'no-cache',
-                    'Expires': '0'
-                }
+            virtualAccountTrxType: "C",
+        },
+        additionalInfo: {
+            channel: "VIRTUAL_ACCOUNT_BCA",
+            virtualAccountConfig: {
+                minAmount: "10000.00",
+                maxAmount: "5000000.00"
             }
-        );
+        }
+    });
+
 }
