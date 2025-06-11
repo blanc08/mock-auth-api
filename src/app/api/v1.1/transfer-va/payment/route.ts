f4fa86666ea2600d0ec69342fbea4ca7a119c883
Bagus Oktaviadi <bagusoktaviadi1@gmail.com>
2025-06-11 15:14:05 +0700

    [update] bill not found scenario

--- src/app/api/v1.1/transfer-va/payment/route.ts
+++ src/app/api/v1.1/transfer-va/payment/route.ts
@@ -4,29 +4,13 @@ export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export async function POST() {
-    return NextResponse.json({
-        responseCode: "2002500",
-        responseMessage: "Success",
-        virtualAccountData: {
-            partnerServiceId: "   19008",
-            customerNo: "00000000000000000001",
-            virtualAccountNo: "  19008000000000000000000001",
-            virtualAccountName: "Customer Name",
-            virtualAccountEmail: "customer.email@mail.com",
-            paymentRequestId: "12839218738127830",
-            paidAmount: {
-                value: "11500.00",
-                currency: "IDR"
+    return NextResponse
+        .json(
+            {
+                responseCode: "4042512",
+                responseMessage: "Bill not found",
             },
-            virtualAccountTrxType: "C",
-        },
-        additionalInfo: {
-            channel: "VIRTUAL_ACCOUNT_BCA",
-            virtualAccountConfig: {
-                minAmount: "10000.00",
-                maxAmount: "5000000.00"
-            }
-        }
-    });
+            { status: 400 }
+        );
 
 }
